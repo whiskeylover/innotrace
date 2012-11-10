ae50b16e3ba1d0336577698c36dcad4f75fd07a1
Ashish Kasturia <ashish@wazo.me>
2012-11-10 12:49:32 -0500

    intial upload

---
+++ innotrace/local_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'innotrace',                      # Or path to database file if using sqlite3.
+        'USER': 'ashish',                      # Not used with sqlite3.
+        'PASSWORD': 'btijti',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+
